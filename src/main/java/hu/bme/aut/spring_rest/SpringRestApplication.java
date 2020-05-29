9158aa3852438a69f7f55f00dad296fe542a315b
Marci <matusekma@gmail.com>
2020-05-29 09:33:26 +0200

    Initial commit

---
+++ src/main/java/hu/bme/aut/spring_rest/SpringRestApplication.java
@@ -0,0 +1,13 @@
+package hu.bme.aut.spring_rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringRestApplication.class, args);
+    }
+
+}
